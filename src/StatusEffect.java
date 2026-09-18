d2ef8b65c93a1069ebf931bec75076fb0ef27341
yagoaraujo781-lang <yagoaraujo781@gmail.com>
2026-09-18 20:55:59 -0300

    Update StatusEffect.java

--- src/StatusEffect.java
+++ src/StatusEffect.java
@@ -1,5 +1,25 @@
 public enum StatusEffect {
-    PARALISADO,
     QUEIMADO,
-    ENVENENADO;
+    ENVENENADO,
+    PARALISADO;
+
+    public void aplicarEfeito(Pokesal pokesal) {
+
+        if (this == QUEIMADO) {
+            double danoQueimadura = pokesal.getHpMaximo() * 0.05; // queimado perde HP e tem o ataque reduzido em 5%
+            pokesal.receberDano(danoQueimadura);
+            System.out.println(pokesal.getNome() + " sofreu " + danoQueimadura + " de dano da queimadura!");
+        }
+
+        if (this == ENVENENADO) { 
+            double danoVeneno = pokesal.getHpMaximo() * 0.08;// veneno tira um dano progressivo de 8%
+            pokesal.receberDano(danoVeneno);
+            System.out.println(pokesal.getNome() + " sofreu " + danoVeneno + " de dano do veneno!");
+        }
+
+        if (this == PARALISADO) {
+            pokesal.setSpd(pokesal.getSpd() * 0.5); // paralisado reduz a velocidade em 50%
+            System.out.println(pokesal.getNome() + " está paralisado e ficou mais lento!");
+        }
+    }
 }
