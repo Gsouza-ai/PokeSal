c36b2ec61d440346ba834d3c567b3ee023fdd69d
yagoaraujo781-lang <yagoaraujo781@gmail.com>
2026-09-18 21:03:33 -0300

    Update Treinador.java

--- src/Treinador.java
+++ src/Treinador.java
@@ -1,19 +1,19 @@
 public class Treinador {
-    private String nome;
+    private String nomeTreinador;
     private PokeSal pokesalInicial;
     private Mochila mochila;
     private boolean usouEsquivaNaRodada;
     private int itensUsadosNaBatalha;
 
-    public Treinador(String nome) {
-        this.nome = nome;
+    public Treinador(String nomeTreinador) {
+        this.nomeTreinador = nomeTreinador;
         this.mochila = new Mochila();
         this.usouEsquivaNaRodada = false;
         this.itensUsadosNaBatalha = 0;
     }
 
     public String getNome() {
-        return nome;
+        return nomeTreinador;
     }
 
     public PokeSal getPokesalInicial() {
@@ -31,13 +31,13 @@ public class Treinador {
     // O treinador escolhe o Pokesal inicial
     public void escolherInicial(PokeSal pokesal) {
         this.pokesalInicial = pokesal;
-        System.out.println(nome + " escolheu " + pokesal.getNome() + " como Pokesal inicial!");
+        System.out.println(nomeTreinador + " escolheu " + pokesal.getNome() + " como Pokesal inicial!");
     }
 
     // Usa um item da mochila no rodadaAtual informado.
     public void usarItem(Item item, int rodadaAtual) {
-        boolean pode = mochila.podeUsarItem(rodadaAtual, itensUsadosNaBatalha);
-        if (!pode) {
+        boolean podeUsarItem = mochila.podeUsarItem(rodadaAtual, itensUsadosNaBatalha);
+        if (!podeUsarItem) {
             return ;
         }
         item.aplicarEfeito(pokesalInicial);
@@ -46,11 +46,11 @@ public class Treinador {
 
     public boolean esquivar() {
         if (usouEsquivaNaRodada) {
-            System.out.println(nome + " já esquivou nesta rodada!");
+            System.out.println(nomeTreinador + " já esquivou nesta rodada!");
             return false;
         }
         usouEsquivaNaRodada = true;
-        System.out.println(nome + " esquivou do ataque!");
+        System.out.println(nomeTreinador + " esquivou do ataque!");
         return true;
     }
 
